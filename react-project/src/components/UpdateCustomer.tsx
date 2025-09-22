62e014c4ba31740073edd741dc674a84dc10f6d8
wasadmin <804anya@gmail.com>
2025-09-22 04:26:37 -0400

    update working with memdb v2

--- react-project/src/components/UpdateCustomer.tsx
+++ react-project/src/components/UpdateCustomer.tsx
@@ -1,74 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import * as memdb from '../../../ProjectAssets/memdb.js';
 import './UpdateCustomer.css';
 
-function UpdateCustomer(props: any) {
-    const navigate = useNavigate();
+function UpdateCustomer() {
     const { id } = useParams();
-    const customerId = Number(id);
-    const selectedCustomer = props.customers.find((c: any) => c.id === customerId);
-    const [customer, setCustomer] = useState(selectedCustomer || {id: customerId, name: '', email: '', password: ''});
-    const cancel = () => {
-        navigate('/');
-    }
+    const navigate = useNavigate();
+    const [customer, setCustomer] = useState({ id: Number(id), name: '', email: '', password: '' });
 
     useEffect(() => {
-        if (selectedCustomer) {
-            setCustomer(selectedCustomer);
+        const existingCustomer = memdb.get(Number(id));
+        if (existingCustomer) {
+            setCustomer({ id: existingCustomer.id, name: existingCustomer.name, email: existingCustomer.email, password: existingCustomer.password });
         }
-    }, [selectedCustomer]);
+    }, [id]);
+
+    const cancel = () => {
+        navigate('/');
+    };
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         const updatedCustomer = {
-            id: customerId,
+            id: Number(id),
             name: customer.name,
             email: customer.email,
             password: customer.password
         };
-        props.updateCustomer(updatedCustomer);
+        memdb.put(Number(updatedCustomer.id), updatedCustomer);
         navigate('/');
     };
 
-    return(
-        <div className='page'>
-            <h2 className='update-customer-title'>Update Customer</h2>
+    return (
+        <div className='container'>
+            <h2 className="update-customer-title">Update Customer</h2>
             <form onSubmit={handleSubmit} className="update-customer-form">
-                <div>
-                    <label htmlFor="name">Name:</label>
+                <div className="form-group">
+                    <label className="label" htmlFor="name">Name:</label>
                     <input
+                        className="input"
                         type="text"
                         id="name"
                         placeholder="Enter your name"
                         value={customer.name}
-                        onChange={(e) => setCustomer({...customer, name: e.target.value})}
+                        onChange={(e) => setCustomer({ ...customer, name: e.target.value })}
+                        required
                     />
                 </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
+                <div className="form-group">
+                    <label className="label" htmlFor="email">Email:</label>
                     <input
-                        type="text"
+                        className="input"
+                        type="email"
                         id="email"
                         placeholder="Enter your email"
                         value={customer.email}
-                        onChange={(e) => setCustomer({...customer, email: e.target.value})}
+                        onChange={(e) => setCustomer({ ...customer, email: e.target.value })}
+                        required
                     />
                 </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
+                <div className="form-group">
+                    <label className="label" htmlFor="password">Password:</label>
                     <input
-                        type="text"
-                        id="Password"
+                        className="input"
+                        type="password"
+                        id="password"
                         placeholder="Enter your password"
                         value={customer.password}
-                        onChange={(e) => setCustomer({...customer, password: e.target.value})}
+                        onChange={(e) => setCustomer({ ...customer, password: e.target.value })}
+                        required
                     />
                 </div>
-                <button type="submit">Update Customer</button>
+                <button className="submit-button" type="submit">Update Customer</button>
             </form>
-            <button className='cancel-button' onClick={cancel}>Cancel</button>
+            <button className="button-cancel" onClick={cancel}>Cancel</button>
         </div>
-    )
+    );
 }
 
 export default UpdateCustomer;

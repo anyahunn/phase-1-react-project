9950465fd9cbc5c785ef20d2d42a4f2248fd5f0c
SergiSalvaPibernat <sergisp97@gmail.com>
2025-09-23 15:26:20 +0200

    Config files update

--- react-project/vite.config.ts
+++ react-project/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { v8Coverage } from '@vitest/coverage-v8'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/setupTests.ts',
+        'src/main.tsx',
+        '**/*.d.ts',
+        'vite.config.ts',
+        'vitest.config.ts'
+      ]
+    }
+  },
 })

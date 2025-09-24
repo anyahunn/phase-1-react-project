bc9bc37200fb113123e1edb7929af950bff1e360
Luke Chester <lukechester03@outlook.com>
2025-09-24 14:39:02 +0100

    added basic toolbar

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import "./App.css";
-import { Container, CssBaseline } from "@mui/material";
+import { AppBar, Container, CssBaseline, Toolbar, Typography } from "@mui/material";
 import DisplayCustomers from "./components/DisplayCustomers";
 import AddCustomer from "./components/AddCustomer";
 import DeleteCustomer from "./components/DeleteCustomer";
@@ -9,14 +9,29 @@ import UpdateCustomer from "./components/UpdateCustomer";
 
 // Extract the routes component for testing
 export const AppRoutes = () => (
-  <Container maxWidth="md" className="App" sx={{ py: 4 }}>
-    <Routes>
-      <Route path="/" element={<DisplayCustomers />} />
-      <Route path="/add_customer/:id" element={<AddCustomer />} />
-      <Route path="/delete_customer/:id" element={<DeleteCustomer />} />
-      <Route path="/update_customer/:id" element={<UpdateCustomer />} />
-    </Routes>
-  </Container>
+  <>
+    <AppBar
+      position="fixed"
+      color="default"
+      elevation={2}
+      sx={{ bgcolor: "background.paper", color: "text.primary" }}
+    >
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Customer Manager
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Toolbar />
+    <Container maxWidth="md" className="App" sx={{ py: 4 }}>
+      <Routes>
+        <Route path="/" element={<DisplayCustomers />} />
+        <Route path="/add_customer/:id" element={<AddCustomer />} />
+        <Route path="/delete_customer/:id" element={<DeleteCustomer />} />
+        <Route path="/update_customer/:id" element={<UpdateCustomer />} />
+      </Routes>
+    </Container>
+  </>
 );
 
 function App() {
